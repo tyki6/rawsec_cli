c9210fa06b2b9e51608dd4866bea6106c99e045d
matthieu Bouamama <matthieu.bouamama@leroymerlin.fr>
2020-12-08 13:38:50 +0100

    add lint stage

--- rawsec_cli/search.py
+++ rawsec_cli/search.py
@@ -10,7 +10,8 @@ def searchProjectTools(json, project):
     listProjects = list()
     for tool in getAllTools(json):
         if ("name" in tool and project.lower() in tool["name"].lower()) or (
-            "description" in tool and project.lower() in tool["description"].lower()
+            "description" in tool
+            and project.lower() in tool["description"].lower()
         ):
             listProjects.append(tool)
     return listProjects
@@ -19,8 +20,11 @@ def searchProjectTools(json, project):
 def searchProjectResources(json, project):
     listProjects = list()
     for resource in getAllResources(json):
-        if ("name" in resource and project.lower() in resource["name"].lower()) or (
-            "description" in resource and project.lower() in resource["description"].lower()
+        if (
+            "name" in resource and project.lower() in resource["name"].lower()
+        ) or (
+            "description" in resource
+            and project.lower() in resource["description"].lower()
         ):
             listProjects.append(resource)
     return listProjects
@@ -30,7 +34,8 @@ def searchProjectCTF(json, project):
     listProjects = list()
     for ctf in getAllCTF(json):
         if ("name" in ctf and project.lower() in ctf["name"].lower()) or (
-            "description" in ctf and project.lower() in ctf["description"].lower()
+            "description" in ctf
+            and project.lower() in ctf["description"].lower()
         ):
             listProjects.append(ctf)
     return listProjects
