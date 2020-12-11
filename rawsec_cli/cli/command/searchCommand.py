80706165efbac07b50d7a8d486e5792593440ac7
matthieu Bouamama <57527739+mBouamama@users.noreply.github.com>
2020-12-11 16:56:16 +0100

    pre-commit.ci (#13)
    * pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * delete double-quote-fixer and add black
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- rawsec_cli/cli/command/searchCommand.py
+++ rawsec_cli/cli/command/searchCommand.py
@@ -8,28 +8,40 @@ from rawsec_cli.search import searchProject
 
 
 @click.command()
-@click.argument('project')
+@click.argument("project")
 @click.pass_context
-@click.option('--lang', '-l', help='Filter by Language')
-@click.option('--price', '-p', is_flag=True, help='Filter by Price=True')
-@click.option('--free', '-f', is_flag=True, help='Filter by Price: Free')
-@click.option('--online', '-on', is_flag=True, help='Filter by Online: True')
+@click.option("--lang", "-l", help="Filter by Language")
+@click.option("--price", "-p", is_flag=True, help="Filter by Price=True")
+@click.option("--free", "-f", is_flag=True, help="Filter by Price: Free")
+@click.option("--online", "-on", is_flag=True, help="Filter by Online: True")
 @click.option(
-    '--offline', '-off', is_flag=True, help='Filter by Online: False',
+    "--offline",
+    "-off",
+    is_flag=True,
+    help="Filter by Online: False",
 )
 @click.option(
-    '--blackarch', '-b', is_flag=True, help='Filter by blackarch: present',
+    "--blackarch",
+    "-b",
+    is_flag=True,
+    help="Filter by blackarch: present",
 )
 def search(ctx, project, lang, price, free, online, offline, blackarch):
-    projects = searchProject(ctx.obj['json'], project)
+    projects = searchProject(ctx.obj["json"], project)
     projects = filterProjects(
-        projects, lang, price, free, online, offline, blackarch,
+        projects,
+        lang,
+        price,
+        free,
+        online,
+        offline,
+        blackarch,
     )
 
     if len(projects) == 1:
-        if 'website' in projects[0]:
-            webbrowser.open_new_tab(projects[0]['website'])
-        elif 'source' in projects[0]:
-            webbrowser.open_new_tab(projects[0]['source'])
+        if "website" in projects[0]:
+            webbrowser.open_new_tab(projects[0]["website"])
+        elif "source" in projects[0]:
+            webbrowser.open_new_tab(projects[0]["source"])
 
-    click.echo(tabulate(projects, 'keys', tablefmt='grid'))
+    click.echo(tabulate(projects, "keys", tablefmt="grid"))
--- tests/test_search.py
+++ tests/test_search.py
@@ -10,46 +10,48 @@ from rawsec_cli.search import searchProjectTools
 class TestSearch(TestCase):
     def setUp(self):
         self.json = {
-            'tools': {'binary_exploitation': {'tools': [{'name': 'tools'}]}},
-            'resources': {
-                'binary_exploitation': {'resources': [{'name': 'resources'}]},
+            "tools": {"binary_exploitation": {"tools": [{"name": "tools"}]}},
+            "resources": {
+                "binary_exploitation": {"resources": [{"name": "resources"}]},
             },
-            'operating_systems': {
-                'binary_exploitation': {
-                    'operating_systems': [{'os': 'operating_systems'}],
+            "operating_systems": {
+                "binary_exploitation": {
+                    "operating_systems": [{"os": "operating_systems"}],
                 },
             },
-            'ctf_platforms': {
-                'binary_exploitation': {
-                    'ctf_platforms': [{'name': 'ctf_platforms'}],
+            "ctf_platforms": {
+                "binary_exploitation": {
+                    "ctf_platforms": [{"name": "ctf_platforms"}],
                 },
             },
         }
 
     def testSearchProjectTools(self):
         self.assertEqual(
-            searchProjectTools(self.json, 'tools'), [{'name': 'tools'}],
+            searchProjectTools(self.json, "tools"),
+            [{"name": "tools"}],
         )
 
     def testSearchProjectResources(self):
         self.assertEqual(
-            searchProjectResources(self.json, 'resources'),
-            [{'name': 'resources'}],
+            searchProjectResources(self.json, "resources"),
+            [{"name": "resources"}],
         )
 
     def testSearchProjectCTF(self):
         self.assertEqual(
-            searchProjectCTF(self.json, 'ctf_platforms'),
-            [{'name': 'ctf_platforms'}],
+            searchProjectCTF(self.json, "ctf_platforms"),
+            [{"name": "ctf_platforms"}],
         )
 
     def testSearchProjectOperating(self):
         self.assertEqual(
-            searchProjectOperating(self.json, 'operating_systems'),
-            [{'os': 'operating_systems'}],
+            searchProjectOperating(self.json, "operating_systems"),
+            [{"os": "operating_systems"}],
         )
 
     def testSearchProject(self):
         self.assertEqual(
-            searchProject(self.json, 'tools'), [{'name': 'tools'}],
+            searchProject(self.json, "tools"),
+            [{"name": "tools"}],
         )
